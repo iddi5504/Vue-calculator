15dfda9fbefd24606c8d0ee5d26ec7e1c6c301ed
Iddi <Iddi5504@gmail.com>
2022-10-25 03:18:15 +0100

    new

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+module.exports = {
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './index/html', to: 'relative/path/to/dest/' }
+    ])
+  ]
+}
 new Vue({
   el: '#app',
   render: h => h(App)
